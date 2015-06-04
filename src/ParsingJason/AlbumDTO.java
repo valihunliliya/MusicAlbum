566c378bf59a1252d63844c47439db72f92ed965
Yaroslav Sydorenko <s.jarko@gmail.com>
2015-06-04 19:29:00 +0300

    commiting initial java project with implementation of jason file parsing.

---
+++ src/ParsingJason/AlbumDTO.java
@@ -0,0 +1,49 @@
+package ParsingJason;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AlbumDTO {
+
+	private String author;
+	private String songname;
+	private Integer duration;
+
+	@JsonCreator
+	public AlbumDTO(@JsonProperty("author") String author,
+			@JsonProperty("songname") String songname,
+			@JsonProperty("duration") Integer duration) {
+		this.author = author;
+		this.songname = songname;
+		this.duration = duration;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getSongname() {
+		return songname;
+	}
+
+	public void setSongname(String songname) {
+		this.songname = songname;
+	}
+
+	public Integer getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Integer duration) {
+		this.duration = duration;
+	}
+
+
+
+}
---
+++ src/ParsingJason/JacksonUsage.java
@@ -0,0 +1,33 @@
+package ParsingJason;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+
+public class JacksonUsage {
+
+	public static void main(String[] args) throws JsonParseException,
+			JsonMappingException, IOException {
+		InputStream input = new FileInputStream("src/album.json");
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		objectMapper
+				.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+		TypeFactory typeFactory = TypeFactory.defaultInstance();
+		List<AlbumDTO> album = objectMapper.readValue(input, typeFactory
+				.constructCollectionType(ArrayList.class, AlbumDTO.class));
+
+		System.out.println(album.get(0).getSongname());
+		System.out.println(album.get(1).getSongname());
+
+	}
+
+}
